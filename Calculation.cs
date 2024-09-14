bc7a5a36eb5b2ac3672e21eea513a2ba813f6e9c
creatior <vartem2020@mail.ru>
2024-09-14 20:33:10 +0300

    v.1.0

---
+++ Calculation.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Calculation
+    {
+        private string[] operations_signs = new string[] { "+", "-", "/", "*" };
+        private string result { get; }
+
+        private int priority(string sign)
+        {
+            switch (sign)
+            {
+                case "*": return 1;
+                case "/": return 1;
+                default : return 0;
+            }
+        }
+
+        private string[] SplitExpression(string expression)
+        {
+            string pattern = @"(\d+|[-+*/])";
+
+            MatchCollection matches = Regex.Matches(expression, pattern);
+
+            string[] result = new string[matches.Count];
+            for (int i = 0; i < matches.Count; i++)
+            {
+                result[i] = matches[i].Value;
+            }
+
+            return result;
+        }
+
+        private List<string> ConvertToRPN(string expression)
+        {
+            Stack<string> stack = new Stack<string>();
+            List<string> result = new List<string>();
+
+            string[] expression_array = SplitExpression(expression);
+
+            foreach (string symbol in expression_array)
+            {
+                if (!operations_signs.Any(sign => symbol == sign))
+                {
+                    result.Add(symbol);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                        stack.Push(symbol);
+                    else
+                    {
+                        while (stack.Count != 0 && priority(symbol) <= priority(stack.Peek()))
+                        {
+                            result.Add(stack.Pop());
+                        }
+                        stack.Push(symbol);
+                    }
+                }
+            }
+
+            while (stack.Count != 0) 
+            { 
+                result.Add(stack.Pop());
+            }
+
+            return result;
+        }
+
+        public string CalculateRPN(string expression)
+        {
+            List<string> rpn_expression = ConvertToRPN(expression);
+
+            Stack<string> stack = new Stack<string>();
+            int a, b;
+
+            foreach (string obj in rpn_expression)
+            {
+                if (!operations_signs.Any(sign => obj == sign))
+                {
+                    stack.Push(obj);
+                }
+                else
+                {
+                    b = Int32.Parse(stack.Pop());
+                    a = Int32.Parse(stack.Pop());
+                    switch (obj)
+                    {
+                        case "+": stack.Push((a + b).ToString()); break;
+                        case "-": stack.Push((a - b).ToString()); break;
+                        case "*": stack.Push((a * b).ToString()); break;
+                        case "/": stack.Push((a / b).ToString()); break;
+                    }
+                }
+            }
+            return stack.Pop();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string[] operations_signs = new string[] {"+","-","*","/"};
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (UIElement element in MainGrid.Children)
+            {
+                if (element is Button)
+                {
+                    ((Button)element).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+            if (MainDisplay.Text.Contains("="))
+                ContinueCalculating(str);
+            if (str == "AC")
+            {
+                MainDisplay.Text = "";
+            }
+            if (str != "=" && str != "AC")
+            {
+                if (operations_signs.Any(sign => str == sign) && operations_signs.Any(sign => MainDisplay.Text.EndsWith(sign)))
+                {
+                    MainDisplay.Text = MainDisplay.Text.Remove(MainDisplay.Text.Length - 1);
+                }
+                MainDisplay.Text += str;
+            }
+            else if (str == "=")
+            {
+                Calculate();
+            }
+        }
+
+        private void ContinueCalculating(string symbol)
+        {
+            if (!operations_signs.Any(sign => symbol == sign))
+            {
+                MainDisplay.Text = "";
+            }
+            else
+            {
+                string[] temp = MainDisplay.Text.Split('=');
+                MainDisplay.Text = "";
+                MainDisplay.Text = temp[1];
+            }
+        }
+
+        private void Calculate()
+        {
+            Calculation result = new Calculation();
+            MainDisplay.Text += $"={result.CalculateRPN(MainDisplay.Text)}";
+        }
+    }
+}
